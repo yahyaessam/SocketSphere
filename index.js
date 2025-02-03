4c26211bee703f732a2b50e26b96697cd4429d57
Yahya Essam <yahyaessam2009@gmai.com>
2025-02-03 20:56:52 +0200

    broadcasting

--- index.js
+++ index.js
@@ -15,12 +15,8 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-  });
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+    io.emit("chat message", msg);
   });
 });
 
