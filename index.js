76537d0883dbff57338e0d7a941432f1c1ed0517
Yahya Essam <yahyaessam2009@gmai.com>
2025-02-03 20:53:01 +0200

    emitting events

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from 'socket.io';
-
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
@@ -11,17 +10,20 @@ const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
